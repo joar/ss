3a9171a791de0d03931fb67174e8de942b5b3413
Joar Wandborg <joar@wandborg.se>
2015-01-23 18:42:56 +0100

    Tuning

--- capture.sh
+++ capture.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 gst-launch-1.0 \
     v4l2src \
-    ! videoscale \
     ! video/x-raw,height=720,width=1280,framerate=30/1 \
-    ! videoconvert \
     ! clockoverlay shaded-background=true font-desc="Monospace 10" \
-    ! vp8enc target-bitrate=8000 \
+    ! vp8enc threads=4 min-quantizer=0 max-quantizer=15 \
     ! m. \
     autoaudiosrc \
     ! vorbisenc \
+    ! queue \
     ! m. \
     webmmux name=m streamable=true \
     ! queue \
-    ! tcpserversink host=localhost port=8888 sync-method=2
+    ! progressreport \
+    ! tcpserversink host=localhost port=8888 sync-method=2 \
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var http = require('http');
 var net = require('net');
+var dns = require('dns');
 
 var app = express();
 var httpServer = http.createServer(app);
 
 
-app.get('/', function (req, res) {
+app.get('/stream', function (req, res) {
     res.writeHead(200, {
         'Date': (new Date()).toUTCString(),
         'Connection': 'close',
@@ -14,15 +15,36 @@ app.get('/', function (req, res) {
         'Content-Type': 'video/webm',
         'Server': 'SS/0.1'
     });
-    var client = new net.Socket();
-    client.connect(8888, '0.0.0.0', function () {
-        console.log('Connected to TCP server');
+
+    var clientIP = req.connection.remoteAddress;
+    var clientHost;
+    var clientSent = 0;
+
+    console.log('Client connected, ip:', clientIP);
+
+    setInterval(function () {
+        console.log('Sent ', clientSent, 'bytes to', clientIP || clientHost);
+    }, 5000)
+
+    dns.reverse(clientIP, function (err, domains) {
+        console.log('Got DNS response for clientIP:', domains, err);
+        var clientHost = domains;
+    })
+
+    var stream = new net.Socket();
+
+    stream.connect(8888, '0.0.0.0', function () {
+        console.log('Connected to TCP stream.');
     });
-    client.on('data', function (data) {
+
+    stream.on('data', function (data) {
+        clientSent += data.length;
         res.write(data);
     })
+
     res.on('close', function () {
-        client.destroy();
+        console.log('Closing connection for client', clientHost || clientIP);
+        stream.destroy();
     })
 });
 
